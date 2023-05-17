91b2a8d95ddc4822b4bee6ccb790653a247282af
Brian Scherner <brianscherner@gmail.com>
2023-05-17 15:33:26 -0700

    Add moreFlavors array and add forEach method for the array.

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,20 @@
 window.addEventListener("load", function() {
 
 	const iceCreamFlavors = ['caramel', 'chocolate chip', 'cookies and cream', 'fudge', 'mint'];
+	const moreFlavors = ['banana', 'strawberry', 'marionberry'];
 	const ul = document.querySelector("ul");
 	iceCreamFlavors.forEach(function(flavor) {
 	const li = document.createElement("li");
 	li.append(flavor);
 	ul.append(li);
 	});
+
+	moreFlavors.forEach(function(flavor) {
+		const li = document.createElement("li");
+		li.append(flavor);
+		ul.prepend(li);
+	});
+
+
 });
+
